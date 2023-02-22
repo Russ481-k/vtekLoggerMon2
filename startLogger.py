0dc2ed35c0fc31f389737a2a2c3f3754927a8063
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2023-02-22 17:44:02 +0900

    자동실행 코드 추가

---
+++ startLogger.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('nohup python3 /home/djkim/vtekLogger/vtekLoggerMon2/loggerDaemon.py&')
---
+++ startWeb.py
@@ -0,0 +1,3 @@
+import os
+
+os.system('nohup sudo python3 /home/djkim/vtekLogger/vtekLoggerMon2/app.py&')
