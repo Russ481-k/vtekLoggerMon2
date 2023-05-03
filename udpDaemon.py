8d366ed77026ee0c760f216e323e91b6c6d781bb
DongJin Kim <31959020+ywydpapa@users.noreply.github.com>
2023-05-03 12:34:00 +0900

    50줄 이상만 처리

--- udpDaemon.py
+++ udpDaemon.py
@@ -33,29 +33,32 @@ while True:
     txtv=''
     sqlv=''
     lc = len(strdata.split(','))
-    for i in range(0,lc):
-        rline = np.append(rline, np.array([strdata.split(',')[i]]))
-        if i== lc-1:
-            sqlv = sqlv + "'" + rline[i] + "'"
-            txtv = txtv + "d" + str('{0:03}'.format(i+1))
-        elif i==0:
-            slicestr = rline[0]
-            slicestr = slicestr[20:]
-            sqlv = sqlv + "'" + slicestr + "',"
-            txtv = txtv + "d" + str('{0:03}'.format(i + 1)) + ','
-        else:
-            sqlv = sqlv + "'" + rline[i] + "',"
-            txtv = txtv + 'd' +str('{0:03}'.format(i+1)) + ','
-    if rline[3] == "SYSTEM":
-        pass
-    else:
-        try:
-            cur = db.cursor()
-            sql = f"INSERT INTO logger.inoutT " +"("+ txtv +")"+ f" VALUES "+"("+ sqlv +")"
-            cur.execute(sql)
-            db.commit()
-            cnt += 1
-            print(cnt)
-        except pymysql.err.InternalError as e:
-            code, msg = e.args
+    if lc > 50:
+        for i in range(0,lc):
+            rline = np.append(rline, np.array([strdata.split(',')[i]]))
+            if i== lc-1:
+                sqlv = sqlv + "'" + rline[i] + "'"
+                txtv = txtv + "d" + str('{0:03}'.format(i+1))
+            elif i==0:
+                slicestr = rline[0]
+                slicestr = slicestr[20:]
+                sqlv = sqlv + "'" + slicestr + "',"
+                txtv = txtv + "d" + str('{0:03}'.format(i + 1)) + ','
+            else:
+                sqlv = sqlv + "'" + rline[i] + "',"
+                txtv = txtv + 'd' +str('{0:03}'.format(i+1)) + ','
+        if rline[3] == "SYSTEM":
             pass
+        else:
+            try:
+                cur = db.cursor()
+                sql = f"INSERT INTO logger.inoutT " +"("+ txtv +")"+ f" VALUES "+"("+ sqlv +")"
+                cur.execute(sql)
+                db.commit()
+                cnt += 1
+                print(cnt)
+            except pymysql.err.InternalError as e:
+                code, msg = e.args
+                pass
+    else:
+        pass
