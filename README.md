7562cc4eeb2874791b06de57c7cd7b2b0f06f721
RussKim <yunsubin481@gmail.com>
2024-07-03 00:15:52 +0900

    change: 설치 자동화 구현

--- README.md
+++ README.md
@@ -1,22 +1,94 @@
+```
+sudo apt-get install git  unzip python3-pip vim mariadb-server
+```
 
+```
+sudo apt install iptables
+```
 
-##############################################
-## 실행 명령어
-##############################################
+```
+sudo pip3 install flask pymysql requests psutil python-dotenv aiohttp schedule numpy
+```
 
-## .env 환경변수 설정
-#주소 및 폴더 디렉토리 설정 필요
+#################################
+# sudo python3 start.py 를 통해 설치 가능 #
+#################################
 
-## mariaDB 확인
-$ mariadb
+```
+sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT
+sudo iptables -I INPUT 1 -p tcp --dport 443 -j ACCEPT
+sudo iptables -I INPUT 1 -p tcp --dport 5654 -j ACCEPT
+```
 
-## Machbase 확인
-$ machbase
+```
+git clone https://github.com/Russ481-k/vtekLoggerMon2.git
+```
 
-## 로거 실행 udpDaemonMachbase.py
-sudo python3 /home/vision/vtekLoggerMon2/stopLogger.py
-sudo python3 /home/vision/vtekLoggerMon2/startLogger.py
-	
-## 어플리케이션 및 일간 주간 합계 스케줄러 실행 chedule_sum.py app.py
-sudo python3 /home/vision/vtekLoggerMon2/stopWeb.py
-sudo python3 /home/vision/vtekLoggerMon2/startWeb.py
+```
+cd vtekLoggerMon2
+```
+
+```
+sudo vi .env
+```
+
+(insert) key click
+
+envhost="0.0.0.0"
+envhostlocal="localhost"
+envuser="root"
+envpassword=""
+envdb="logger"
+envcharset="utf8"
+
+(esc)
+
+:wq!
+
+```
+sudo mysql
+```
+
+```
+CREATE DATABASE logger;
+GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;
+CREATE USER 'vtek'@'localhost' IDENTIFIED BY 'core2020';
+GRANT ALL PRIVILEGES ON *.* TO 'vtek'@'localhost' WITH GRANT OPTION;
+FLUSH PRIVILEGES;
+EXIT;
+```
+
+```
+sudo mysql -u root -p logger<./sql/userAccount.sql
+sudo mysql -u root -p logger<./sql/menuCustom.sql
+```
+
+```
+sh -c "$(curl -fsSL https://docs.machbase.com/install.sh)"
+```
+#################################################
+# Download complete machbase-neo-v8.0.21-linux-amd64.zip #  => 버전 정보와 환경과 주소를 이후 코드에 맞춰줘야함
+#################################################
+
+```
+unzip machbase-neo-v8.0.21-linux-amd64.zip
+```
+
+```
+./machbase-neo-v8.0.21-linux-amd64/machbase-neo serve --host 192.168.1.47 --daemon --pid ./machbase-neo.pid
+```
+
+```
+sudo python3 ./iniTable.py
+```
+
+```
+nohup python3 udpDaemonMachbase.py & echo $! > ./udpDaemonMachbase.piad
+```
+
+```
+sudo python3 stopLogger.py
+sudo python3 startLogger.py
+sudo python3 stopWeb.py
+sudo python3 startWeb.py
+```
--- start.py
+++ start.py
@@ -7,221 +7,89 @@ import platform
 # .env 파일 로드
 load_dotenv()
 
+envhost = os.getenv('envhost')
 
 def run_command(command):
-    result = subprocess.run(command, shell=True,
-                            capture_output=True, text=True)
+    result = subprocess.run(command, shell=True, capture_output=True, text=True)
     if result.returncode != 0:
         print(f"명령어 실행 오류: {command}")
         print(f"출력: {result.stdout}")
         print(f"에러: {result.stderr}")
+        return False
     else:
         print(f"명령어 실행 성공: {command}")
         print(f"출력: {result.stdout}")
-    return result
+        return True
 
+# 패키지 설치
+if not run_command('sudo apt-get install -y git unzip python3-pip vim mariadb-server'):
+    exit(1)
+if not run_command('sudo apt install -y iptables'):
+    exit(1)
+if not run_command('sudo pip3 install -y flask pymysql requests psutil python-dotenv aiohttp schedule numpy'):
+    exit(1)
 
-# 1. 깃허브 파일 로드 및 파일 권한 설정
-if run_command('sudo chown -R vtek ./').returncode != 0:
+# iptables 설정
+if not run_command('sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT'):
+    exit(1)
+if not run_command('sudo iptables -I INPUT 1 -p tcp --dport 443 -j ACCEPT'):
+    exit(1)
+if not run_command('sudo iptables -I INPUT 1 -p tcp --dport 5654 -j ACCEPT'):
+    exit(1)
+
+# 깃허브 파일 로드 및 파일 권한 설정
+if not run_command('git clone https://github.com/Russ481-k/vtekLoggerMon2.git'):
+    exit(1)
+if not run_command('sudo chown -R vtek ./vtekLoggerMon2'):
     print("깃허브 파일 권한 설정 중 오류가 발생했습니다.")
+    exit(1)
 else:
     print("깃허브 파일 권한 설정 성공.")
 
-# 2. git clone
-if run_command('git clone https://github.com/Russ481-k/vtekLoggerMon2.git').returncode != 0:
-    print("깃허브 리포지토리 클론 중 오류가 발생했습니다.")
-else:
-    print("깃허브 리포지토리 클론 성공.")
-
-# 3. cd vtekLoggerMon2
-try:
-    os.chdir('vtekLoggerMon2')
-    print("디렉토리 변경 성공: vtekLoggerMon2")
-except Exception as e:
-    print(f"디렉토리 변경 중 오류가 발생했습니다: {e}")
-
-# 4. .env 설정
-env_content = """
-envhost="192.168.1.47"
-envhostlocal="localhost"
-envuser="vtek"
-envpassword="core2020"
-envdb="logger"
-envcharset="utf8"
-"""
-try:
-    with open('.env', 'w') as env_file:
-        env_file.write(env_content)
-    print(".env 파일 생성 성공.")
-except Exception as e:
-    print(f".env 파일 생성 중 오류가 발생했습니다: {e}")
-
-
-# 6. MariaDB 설치 및 설정
-
-
-
-if run_command('sudo apt update').returncode != 0:
-    print("APT 업데이트 중 오류가 발생했습니다.")
-else:
-    print("APT 업데이트 성공.")
-
-if run_command('sudo apt-get install git unzip python3-pip vim').returncode != 0:
-    print("APT 의존성 모듈 설치 중 오류가 발생했습니다.")
-else:
-    print("APT 의존성 모듈 설치 성공.")
-
-if run_command('sudo pip3 install flask pymysql requests psutil python-dotenv aiohttp schedule numpy').returncode != 0:
-    print("pip 의존성 모듈 설치 중 오류가 발생했습니다.")
-else:
-    print("pip 의존성 모듈 설치 성공.")
-
-if run_command('sudo apt install mariadb-server --fix-missing --fix-broken').returncode != 0:
-    print("MariaDB 설치 중 오류가 발생했습니다.")
-else:
-    print("MariaDB 설치 성공.")
-
-if run_command('sudo systemctl stop mariadb').returncode != 0:
-    print("MariaDB 중지 중 오류가 발생했습니다.")
-else:
-    print("MariaDB 중지 성공.")
-
-if run_command('sudo systemctl start mariadb').returncode != 0:
-    print("MariaDB 시작 중 오류가 발생했습니다.")
-else:
-    print("MariaDB 시작 성공.")
-
-# # 디렉토리 권한 설정
-# if run_command('sudo chown -R $(whoami) /var/lib/mysql').returncode != 0:
-#     print("디렉토리 소유권 변경 중 오류가 발생했습니다.")
-# else:
-#     print("디렉토리 소유권 변경 성공.")
-
-# if run_command('sudo chmod -R 755 /var/lib/mysql').returncode != 0:
-#     print("디렉토리 권한 변경 중 오류가 발생했습니다.")
-# else:
-#     print("디렉토리 권한 변경 성공.")
+# .env 파일 생성
+os.chdir('vtekLoggerMon2')
+with open('.env', 'w') as f:
+    f.write(f"envhost='0.0.0.0'\n")
+    f.write(f"envhostlocal='localhost'\n")
+    f.write(f"envuser='root'\n")
+    f.write(f"envpassword=''\n")
+    f.write(f"envdb='logger'\n")
+    f.write(f"envcharset='utf8'\n")
 
 # MariaDB 설정
-# try:
-#     if run_command('mysql -u root -p GRANT ALL PRIVILEGES ON *.* TO root@localhost WITH GRANT OPTION;').returncode != 0:
-#         print("MariaDB 시작 중 오류가 발생했습니다.")
-#     else:
-#         print("MariaDB 시작 성공.")
-#     connection = pymysql.connect(
-#         host=os.getenv('envhostlocal'),
-#         user=os.getenv('envuser'),
-#         password=os.getenv('envpassword'),
-#         charset=os.getenv('envcharset')
-#     )
-#     cursor = connection.cursor()
-#     cursor.execute(
-#         "GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost' WITH GRANT OPTION;")
-#     cursor.execute("CREATE DATABASE IF NOT EXISTS logger;")
-#     cursor.execute("USE logger;")
-#     cursor.execute("FLUSH PRIVILEGES;")
-    
-#     connection.commit()
-#     print("MariaDB 설정 성공.")
-# except pymysql.MySQLError as e:
-#     print(f"MariaDB 설정 중 오류가 발생했습니다: {e}")
-# finally:
-#     cursor.close()
-#     connection.close()
+if not run_command('sudo mysql -e "CREATE DATABASE logger;"'):
+    exit(1)
+if not run_command('sudo mysql -e "GRANT ALL PRIVILEGES ON . TO \'root\'@\'localhost\' WITH GRANT OPTION;"'):
+    exit(1)
+if not run_command('sudo mysql -e "CREATE USER \'vtek\'@\'localhost\' IDENTIFIED BY \'core2020\';"'):
+    exit(1)
+if not run_command('sudo mysql -e "GRANT ALL PRIVILEGES ON . TO \'vtek\'@\'localhost\' WITH GRANT OPTION;"'):
+    exit(1)
+if not run_command('sudo mysql -e "FLUSH PRIVILEGES;"'):
+    exit(1)
+if not run_command('sudo mysql -u root -p logger < ./sql/userAccount.sql'):
+    exit(1)
+if not run_command('sudo mysql -u root -p logger < ./sql/menuCustom.sql'):
+    exit(1)
 
-# # SQL 파일 실행
-# sql_files = ['./sql/userAccount.sql', './sql/menuCustom.sql']
-# for sql_file in sql_files:
-#     try:
-#         connection = pymysql.connect(
-#             host=os.getenv('envhostlocal'),
-#             user=os.getenv('envuser'),
-#             password=os.getenv('envpassword'),
-#             database='logger',
-#             charset=os.getenv('envcharset')
-#         )
-#         cursor = connection.cursor()
-#         with open(sql_file, 'r') as file:
-#             sql = file.read()
-#             cursor.execute(sql)
-#             connection.commit()
-#             print(f"SQL 파일 실행 성공: {sql_file}")
-#     except Exception as e:
-#         print(f"SQL 파일 실행 실패: {sql_file}, 오류: {e}")
-#     finally:
-#         cursor.close()
-#         connection.close()
-            
+# Machbase 설치
+if not run_command('sh -c "$(curl -fsSL https://docs.machbase.com/install.sh)"'):
+    exit(1)
+if not run_command('unzip machbase-neo-v8.0.21-linux-amd64.zip'):
+    exit(1)
+if not run_command(f'./machbase-neo-v8.0.21-linux-amd64/machbase-neo serve --host {envhost} --daemon --pid ./machbase-neo.pid'):
+    exit(1)
 
-if run_command('sh -c "$(curl -fsSL https://docs.machbase.com/install.sh)"').returncode != 0:
-    print("Machbase 설치 중 오류가 발생했습니다.")
-else:
-    print("Machbase 설치 성공.")
-
-if run_command('unzip machbase-neo-v8.0.20-linux-amd64.zip').returncode != 0:
-    print("Machbase 압축 해제 중 오류가 발생했습니다.")
-else:
-    print("Machbase 압축 해제 성공.")
-
-if run_command(f'./machbase-neo-v8.0.20-linux-amd64/machbase-neo serve --host 192.168.1.47 --daemon --pid ./machbase-neo.pid').returncode != 0:
-    print("Machbase 실행 중 오류가 발생했습니다.")
-else:
-    print("Machbase 실행 성공.")
-
-
-#사용 포트 개방
-if run_command('sudo apt install iptables').returncode != 0:
-    print("iptables 설치 중 오류가 발생했습니다.")
-else:
-    print("iptables 설치 성공.")
-
-\if run_command('sudo iptables -I INPUT 1 -p tcp --dport 80 -j ACCEPT').returncode != 0:
-    print("80포트 개방 중 오류가 발생했습니다.")
-else:
-    print("80포트 개방 성공.")
-
-if run_command('sudo iptables -I INPUT 1 -p tcp --dport 443 -j ACCEPT').returncode != 0:
-    print("443포트 개방 중 오류가 발생했습니다.")
-else:
-    print("443포트 개방 성공.")
-
-if run_command('sudo iptables -I INPUT 1 -p tcp --dport 5654 -j ACCEPT').returncode != 0:
-    print("5654포트 개방 중 오류가 발생했습니다.")
-else:
-    print("5654포트 개방 성공.")
-
-
-# 8. 초기 테이블 생성
-if run_command('sudo python3 ./iniTable.py').returncode != 0:
-    print("초기 테이블 생성 중 오류가 발생했습니다.")
-else:
-    print("초기 테이블 생성 성공.")
-
-# # 9. 로거 프로세스 실행/중지
-# if run_command('sudo python3 ./stopLogger.py').returncode != 0:
-#     print("로거 프로세스 중지 중 오류가 발생했습니다.")
-# else:
-#     print("로거 프로세스 중지 성공.")
-
-# if run_command('sudo python3 ./startLogger.py').returncode != 0:
-#     print("로거 프로세스 실행 중 오류가 발생했습니다.")
-# else:
-#     print("로거 프로세스 실행 성공.")
-
-# # 10. 스케줄러 실행
-# if run_command('sudo python3 ./schedule_sum.py').returncode != 0:
-#     print("스케줄러 실행 중 오류가 발생했습니다.")
-# else:
-#     print("스케줄러 실행 성공.")
-
-# # 11. 웹 프로세스 실행/중지
-# if run_command('sudo python3 ./stopWeb.py').returncode != 0:
-#     print("웹 프로세스 중지 중 오류가 발생했습니다.")
-# else:
-#     print("웹 프로세스 중지 성공.")
-
-# if run_command('sudo python3 ./startWeb.py').returncode != 0:
-#     print("웹 프로세스 실행 중 오류가 발생했습니다.")
-# else:
-#     print("웹 프로세스 실행 성공.")
-
+# 초기화 및 실행
+if not run_command('sudo python3 ./iniTable.py'):
+    exit(1)
+if not run_command('nohup python3 udpDaemonMachbase.py & echo $! > ./udpDaemonMachbase.pid'):
+    exit(1)
+if not run_command('sudo python3 stopLogger.py'):
+    exit(1)
+if not run_command('sudo python3 startLogger.py'):
+    exit(1)
+if not run_command('sudo python3 stopWeb.py'):
+    exit(1)
+if not run_command('sudo python3 startWeb.py'):
+    exit(1)
