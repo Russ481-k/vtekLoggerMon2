723b9d9f624519e149d52dedc802a8780e36e44c
yunsubin <yunsubin481@gmail.com>
2024-07-02 22:43:18 +0900

    Update startWeb.py

--- startWeb.py
+++ startWeb.py
@@ -1,10 +1,5 @@
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-folderDirectory = os.getenv('folderdirectory')
 
 # 프로세스 시작
-os.system(f'nohup sudo python3 {folderDirectory}/vtekLoggerMon2/app.py&')
-os.system(f'nohup sudo python3 {folderDirectory}/vtekLoggerMon2/schedule_sum.py&')
+os.system(f'nohup sudo python3 ./app.py&')
+os.system(f'nohup sudo python3 ./schedule_sum.py&')
